5182aee9bb1e6804604c2df1749520e435f60189
andre matheus lima <matheus3232@gmail.com>
2023-03-31 17:24:26 -0300

    projeto jogo da memoria

---
+++ script.js
@@ -0,0 +1,80 @@
+const FRONT = 'cardFront';
+const BACK = 'cardBack';
+const CARD = 'card';
+const ICON = 'icon'
+
+startGame();
+
+function startGame() {
+    initializeCards(game.createCard())
+}
+
+function initializeCards(cards) {
+    let gameBoard = document.getElementById('gameBoard');
+    gameBoard.innerHTML = '' ;
+    game.cards.forEach(card => {
+
+        let cardElement = document.createElement('div');
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+
+        createCardContent(card, cardElement)
+
+        cardElement.addEventListener('click', flippedCard)
+        gameBoard.appendChild(cardElement)
+
+    })
+}
+function createCardContent(card, cardElement) {
+
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
+}
+
+function createCardFace(face, card, element) {
+
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face)
+
+    if (face === FRONT) {
+        let imgElement = document.createElement('img');
+        imgElement.classList.add(ICON);
+        imgElement.src = './assets/images/' + card.icon + '.png';
+        cardElementFace.appendChild(imgElement);
+    } else {
+        cardElementFace.innerHTML = '&lt/&gt';
+    }
+    element.appendChild(cardElementFace);
+}
+
+function flippedCard() {
+
+    if (game.setCard(this.id)) {
+        this.classList.add('flip');
+        if (game.secondCard) {
+            if (game.checkMatch()) {
+                game.clearCards();
+                if (game.checkGameOver()) {
+                    let gameOverSlide = document.getElementById('gameOver')
+                    gameOverSlide.style.display = 'flex';
+                }
+            } else {
+                setTimeout(() => {
+                    let firstCardView = document.getElementById(game.firstCard.id);
+                    let secondCardView = document.getElementById(game.secondCard.id);
+
+                    firstCardView.classList.remove('flip')
+                    secondCardView.classList.remove('flip')
+                    game.unflipCards();
+                }, 1000)
+            }
+        }
+    }
+}
+function restart () {
+    game.clearCards();
+    startGame();
+    let gameOverSlide = document.getElementById('gameOver');
+    gameOverSlide.style.display = 'none' ;
+}
